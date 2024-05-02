ef550cbb13db4368463a6dababbb47e2dc80802d
Shivan Sivakumaran <me@shivan.xyz>
2024-05-03 00:07:31 +1200

    feat: initial mvp

--- cmd/root.go
+++ cmd/root.go
@@ -3,16 +3,42 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "makegif",
 	Short: "makegif turns movies files into gifs.",
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Echo: ", strings.Join(args, " "))
+		for _, arg := range args {
+			var err error
+			parent := filepath.Dir(arg)
+			palleteDir := filepath.Join(parent, ".temp.png")
+			err = ffmpeg.Input(arg).
+				Output(palleteDir, ffmpeg.KwArgs{"vf": "palettegen"}).
+				OverWriteOutput().ErrorToStdOut().Run()
+			if err != nil {
+				panic(err)
+			}
+			err = ffmpeg.Input(arg).
+				Output(strings.TrimSuffix(arg, filepath.Ext(arg))+".gif", ffmpeg.KwArgs{
+					"i":              ".temp.png",
+					"filter_complex": "fps=10,scale=1280:-1[x];[x][1:v]paletteuse"}).
+				OverWriteOutput().Run()
+			if err != nil {
+				panic(err)
+			}
+			err = os.Remove(palleteDir)
+			if err != nil {
+				panic(err)
+			}
+		}
+
 	},
 }
 
